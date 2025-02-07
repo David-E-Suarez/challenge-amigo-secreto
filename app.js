1fb9ff7d338780721c4dc7d01cd0ed465b3133dd
David-E-Suarez <suarez.david.emmanuel@gmail.com>
2025-02-07 15:04:01 -0600

    Se agrega la funcion actualizar lista, limpar caja y se la alerta en caso que se repita un nombre.

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ function asignarTextoElemento(elemento, texto) {
     return;
 }
 
-function agregarAmigo(){
-    let nombreAmigo = document.getElementById('amigo').value.trim();
-
-    if (nombreAmigo === ""){
-        alert('Por favor, introduce un nombre valido');
-    }else{
-        listaAmigos.push(nombreAmigo);
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
     }
+    
+    if (listaAmigos.includes(nombre)) {
+        alert("Este nombre ya ha sido agregado.");
+        return;
+    }
+    
+    listaAmigos.push(nombre);
+    actualizarListaAmigos();
+    limpiarCaja();
+    
+}
 
-}   
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+
+function actualizarListaAmigos() {
+    let listaNombres = document.getElementById("listaAmigos");
+    listaNombres.innerHTML = "";
+    
+    listaAmigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaNombres.appendChild(li);
+    });
+}
