fe2b448fb0994bc81356b21775c763414e02d095
David-E-Suarez <suarez.david.emmanuel@gmail.com>
2025-02-07 13:31:04 -0600

    Se agrega la función agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-let amigos = [];
+let listaAmigos = [];
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value.trim();
+
+    if (nombreAmigo === ""){
+        alert('Por favor, introduce un nombre valido');
+    }else{
+        listaAmigos.push(nombreAmigo);
+    }
+
+}   
